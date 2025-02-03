549ccc14510a00456600bf12beba6bf87724f75f
Rustem Sankubayev <senku.kz@gmail.com>
2025-02-03 10:50:57 +0500

    add actuator health check permit

--- src/main/java/com/example/configservice/config/SecurityBeans.java
+++ src/main/java/com/example/configservice/config/SecurityBeans.java
@@ -6,14 +6,17 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityBeans {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         return httpSecurity
-                .authorizeHttpRequests(customizer -> customizer.anyRequest().authenticated())
+                .authorizeHttpRequests(customizer -> customizer
+                                .requestMatchers("/actuator/health").permitAll() // Разрешаем доступ к health-check без авторизации
+                                .anyRequest().authenticated()
+                )
                 .csrf(CsrfConfigurer::disable)
                 .sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .httpBasic(Customizer.withDefaults())
