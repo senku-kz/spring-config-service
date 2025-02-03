b90cd66c3ab0293c2c29809df582f8fb74a7f55a
Rustem Sankubayev <senku.kz@gmail.com>
2025-02-03 10:32:13 +0500

    add basic security

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM maven:3.8.5-openjdk-17 AS build
 WORKDIR /app
 COPY pom.xml .
 COPY src ./src
-COPY settings.xml /root/.m2/settings.xml
+#COPY settings.xml /root/.m2/settings.xml
 RUN mvn clean package -DskipTests
 
 # Stage 2: Package
---
+++ src/main/java/com/example/configservice/config/SecurityBeans.java
@@ -0,0 +1,23 @@
+package com.example.configservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;;
+
+@Configuration
+public class SecurityBeans {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        return httpSecurity
+                .authorizeHttpRequests(customizer -> customizer.anyRequest().authenticated())
+                .csrf(CsrfConfigurer::disable)
+                .sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+    
+}
